0307979644dd0e43b0685d22b5ffdb9a5fe7f5d3
Ilya <ifed@tuta.io>
2018-06-29 23:20:29 +0300

    feat: add templatizer function
    Includes the following implementation:
    - template import
    - writing options with "args:" or w/o prefix
    
    Closes #13

--- src/templatizer.js
+++ src/templatizer.js
@@ -3,7 +3,20 @@ const templatizer = (options) => {
     const optionTemplate = require('.template/option.json');
 
     let result = argumentsTemplate;
-    //TODO: create implementation of templatizer function
+    options.forEach((option) => {
+        let optionSchema = optionTemplate.slice();
+        const argumentName = option[1];
+        switch (argumentName){
+            //TODO: arguments with prefix linterhub:
+            default:
+                optionSchema.id = (option[2] === 1 ? "args:" : "") + option[1];
+                //TODO: argument types
+                optionSchema.type = "string";
+                optionSchema.description = option[4];
+                optionSchema.default = option[3] ? option[3] : "";
+        }
+        result.definitions.arguments.properties[argumentName] = optionSchema;
+    });
 
     return result;
 };
