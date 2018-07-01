b813720b3aafbe02992421da5db956e4677837f4
Ilya <ifed@tuta.io>
2018-07-01 18:00:58 +0300

    feat: add handle function
    - add implementation of handle function
    - add functions parseSection, splitSection, parseOption (w/o implementation)
    
    Closes #18

--- src/handle.js
+++ src/handle.js
@@ -1,7 +1,32 @@
 const handle = (help) => {
-    // TODO: create implementation of parse function
+    const sectionName = "Options:";
     let options = [];
+
+    parseSection(sectionName, help).forEach(section => {
+        splitSection(section).forEach(option => {
+            if (option.indexOf("-") === 0) {
+                options.push(parseOption(option));
+            }
+        });
+    });
     return options;
+};
+
+const parseSection = (sectionName, help) => {
+    //TODO: create implementation
+    let section = [];
+    return section;
+};
+
+const splitSection = (section) => {
+    //TODO: create implementation
+    let split = [];
+    return split;
+};
+
+const parseOption = (option) => {
+    //TODO: create implementation
+    return [];
 };
 
 module.exports = handle;
