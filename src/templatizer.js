5ffeb9cec1ac4f858204cf5a101f2902508c1cec
Ilya <ifed@tuta.io>
2018-07-13 01:25:19 +0300

    feat: add implementation of parseOption function
    Closes #17

--- src/templatizer.js
+++ src/templatizer.js
@@ -1,19 +1,19 @@
 const templatizer = (options) => {
     const argumentsTemplate = require('./template/args.json');
-    const optionTemplate = require('.template/option.json');
+    const optionTemplate = require('./template/option.json');
 
     let result = argumentsTemplate;
-    options.forEach((option) => {
-        let optionSchema = optionTemplate.slice();
-        const argumentName = option[1];
+    options.map(option => {
+        let optionSchema = Object.assign({}, optionTemplate);
+        const argumentName = option.longName;
         switch (argumentName){
             //TODO: arguments with prefix linterhub:
             default:
-                optionSchema.id = (option[2] === 1 ? "args:" : "") + option[1];
+                optionSchema.id = option.longName;
                 //TODO: argument types
                 optionSchema.type = "string";
-                optionSchema.description = option[4];
-                optionSchema.default = option[3] ? option[3] : "";
+                optionSchema.description = option.description;
+                optionSchema.default = option.defaultValue;
         }
         result.definitions.arguments.properties[argumentName] = optionSchema;
     });
