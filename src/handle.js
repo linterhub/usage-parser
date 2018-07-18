73f2c3ba65d76057f18ab1553fa59fbb39d65449
Ilya <ifed@tuta.io>
2018-07-18 14:38:05 +0300

    refactoring: fix errors of linting
    Closes #36

--- src/handle.js
+++ src/handle.js
@@ -5,42 +5,44 @@ const argumentTemplate = {
     longName: null,
     isFlag: true,
     default: false,
-    description: null
+    description: null,
 };
 
 const handle = (help) => {
     const context = {
         section: [
             {
-                name: "Options:",
+                name: 'Options:',
                 data: [],
                 required: true,
-                func: parseOptions
+                func: parseOptions,
             },
             {
-                name: "Usage:",
+                name: 'Usage:',
                 data: [],
                 required: false,
-                func: parseUsage
+                func: parseUsage,
             },
             {
-                name: "Examples:",
+                name: 'Examples:',
                 data: [],
                 required: false,
-                func: parseExamples
+                func: parseExamples,
             },
         ],
         options: [],
-        delimiter: ""
+        delimiter: '',
     };
 
     try {
-        context.section.forEach(section => {
+        context.section.forEach((section) => {
             section.data = parseSection(section.name, help);
             if (section.data) {
-                section.data.forEach(data => section.func(data, context));
+                section.data.forEach((data) => section.func(data, context));
             } else if (section.required) {
-                throw `Required section ${section.name} not found`;
+                const sectionNotFound
+                    = new Error(`Required section ${section.name} not found`);
+                throw sectionNotFound;
             }
         });
         return context;
@@ -51,12 +53,13 @@ const handle = (help) => {
 
 const parseOptions = (section, context) => {
     try {
-        splitSection(section).forEach(option => {
-            if (option.indexOf("-") === 0) {
+        splitSection(section).forEach((option) => {
+            if (option.indexOf('-') === 0) {
                 let argument = Object.assign({}, argumentTemplate);
 
                 option.trim().split(/\s\s+/, 2).map((section, index) => {
-                    index === 0 ? setArgument(section, argument) : setDescription(section, argument);
+                    index === 0 ? setArgument(section, argument)
+                        : setDescription(section, argument);
                 });
                 context.options.push(argument);
             }
@@ -68,25 +71,27 @@ const parseOptions = (section, context) => {
 
 const parseUsage = (section, context) => {
     let argument = Object.assign({}, argumentTemplate);
-    argument.longName = "";
+    argument.longName = '';
     context.options.push(section.match(/file|path/i) ? argument : {});
 };
 
 const parseExamples = (section, context) => {
-    const delimiterDefault = "=";
-    const delimitersTemplate = [" ", "="];
-    delimitersTemplate.forEach(delimiter => {
+    const delimiterDefault = '=';
+    const delimitersTemplate = [' ', '='];
+    delimitersTemplate.forEach((delimiter) => {
         const regularExp = new RegExp(`-[^ \t\n]+${delimiter}[^ \t\n-]`);
-        section.match(regularExp) ? context.delimiter = delimiter : "";
+        section.match(regularExp) ? context.delimiter = delimiter : '';
     });
-    context.delimiter = context.delimiter ? context.delimiter : delimiterDefault;
+    context.delimiter =
+        context.delimiter ? context.delimiter : delimiterDefault;
 };
 
 const parseSection = (sectionName, help) => {
     try {
-        const regularExp = new RegExp(`[^\n]*${sectionName}[^\n]*\n?(?:[ \t].*?(?:\n|$))*`);
+        const regularExp =
+            new RegExp(`[^\n]*${sectionName}[^\n]*\n?(?:[ \t].*?(?:\n|$))*`);
         const matches = help.match(regularExp);
-        return matches ? matches.map(match => match.trim()) : [];
+        return matches ? matches.map((match) => match.trim()) : [];
     } catch (error) {
         throw error;
     }
@@ -95,14 +100,14 @@ const parseSection = (sectionName, help) => {
 const splitSection = (section) => {
     try {
         let result = [];
-        const strings = section.split("\n").slice(1);
+        const strings = section.split('\n').slice(1);
 
         let j = 0;
 
-        strings.forEach(str => {
+        strings.forEach((str) => {
             str = str.trim();
 
-            if (str.indexOf("-") === 0) {
+            if (str.indexOf('-') === 0) {
                 result.push(str);
                 j++;
             } else if (j > 0) {
@@ -119,13 +124,13 @@ const splitSection = (section) => {
 const setArgument = (section, argument) => {
     const argumentPrefix = /^-*/i;
 
-    section = RemoveExtraCharacters(section);
-    section.split(/\s+/).map(arg => {
-        switch(argumentPrefix.exec(arg).toString()) {
-            case "--":
+    section = removeExtraCharacters(section);
+    section.split(/\s+/).map((arg) => {
+        switch (argumentPrefix.exec(arg).toString()) {
+            case '--':
                 argument.longName = arg;
                 break;
-            case "-":
+            case '-':
                 argument.shortName = arg;
                 break;
             default:
@@ -137,7 +142,7 @@ const setArgument = (section, argument) => {
 
 const setDescription = (section, argument) => {
     argument.description = section;
-    //TODO: add other patterns for default value parsing
+    // TODO: add other patterns for default value parsing
     const defaultValues = section.match(/\[default: (.*?)\]/i);
     if (defaultValues) {
         argument.defaultValue = defaultValues[1];
@@ -145,9 +150,9 @@ const setDescription = (section, argument) => {
     }
 };
 
-const RemoveExtraCharacters = (args) => {
-    args = args.replace(/=/g, " ");
-    args = args.replace(/,/g, " ");
+const removeExtraCharacters = (args) => {
+    args = args.replace(/=/g, ' ');
+    args = args.replace(/,/g, ' ');
     return args;
 };
 
--- src/templatizer.js
+++ src/templatizer.js
@@ -5,35 +5,36 @@ const templatizer = (context) => {
     const optionTemplate = require('./template/option.json');
 
     let result = argumentsTemplate;
-    context.options.map(option => {
+    context.options.map((option) => {
         let optionSchema = Object.assign({}, optionTemplate);
         const argumentName = option.longName;
-        switch (argumentName){
-            case "--version":
-                optionSchema.id = "linterhub:version";
-                optionSchema.type = "null";
+        switch (argumentName) {
+            case '--version':
+                optionSchema.id = 'linterhub:version';
+                optionSchema.type = 'null';
                 break;
-            case "--help":
-                optionSchema.id = "linterhub:help";
-                optionSchema.type = "null";
+            case '--help':
+                optionSchema.id = 'linterhub:help';
+                optionSchema.type = 'null';
                 break;
-            case "--config":
-                optionSchema.id = "linterhub:config";
+            case '--config':
+                optionSchema.id = 'linterhub:config';
                 break;
-            case "--stdin":
-                optionSchema.id = "linterhub:stdin";
+            case '--stdin':
+                optionSchema.id = 'linterhub:stdin';
                 break;
-            case "--stdin-filename":
-            case "--stdin-filepath":
-                optionSchema.id = "linterhub:filename";
+            case '--stdin-filename':
+            case '--stdin-filepath':
+                optionSchema.id = 'linterhub:filename';
                 break;
-            case "":
-                optionSchema.id = "linterhub:path";
-                optionSchema.description = "Path to file or folder to analyze";
+            case '':
+                optionSchema.id = 'linterhub:path';
+                optionSchema.description = 'Path to file or folder to analyze';
                 break;
             default:
-                optionSchema.id = (!option.isFlag ? "args:" : "") + option.longName;
-                //TODO: argument types
+                optionSchema.id = (!option.isFlag ? 'args:' : '')
+                    + option.longName;
+                // TODO: argument types
         }
         optionSchema.description = option.description;
         optionSchema.default = option.default;
