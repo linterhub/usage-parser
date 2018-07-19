8a437f1e661242ef8c8eff9b034fe88ef6307d9e
Ilya <ifed@tuta.io>
2018-07-19 15:14:16 +0300

    refactoring: moved configuration to a separate files
    Templates was moved to:
    - template/argument.json
    - template/context.js
    
    Configuration was separated from context and moved to:
    - template/config.json
    
    Functions parseSection, parseUsage, parseExamples now exports to template/context.js and was moved to:
    - parseSection.js
    
    Functions setArgument, setDesctription was moved to:
    - argumentsFill.js
    
    Function parseSection was renamed to findSection
    
    Closes #39

---
+++ src/argumentsFill.js
@@ -0,0 +1,38 @@
+const setArgument = (section, argument) => {
+    const argumentPrefix = /^-*/i;
+
+    section = removeExtraCharacters(section);
+    section.split(/\s+/).map((arg) => {
+        switch (argumentPrefix.exec(arg).toString()) {
+            case '--':
+                argument.longName = arg;
+                break;
+            case '-':
+                argument.shortName = arg;
+                break;
+            default:
+                argument.isFlag = false;
+                break;
+        }
+    });
+};
+
+const setDescription = (section, argument) => {
+    argument.description = section;
+    // TODO: add other patterns for default value parsing
+    const defaultValues = section.match(/\[default: (.*?)\]/i);
+    if (defaultValues) {
+        argument.defaultValue = defaultValues[1];
+        argument.isFlag = false;
+    }
+};
+
+const removeExtraCharacters = (args) => {
+    args = args.replace(/=/g, ' ');
+    args = args.replace(/,/g, ' ');
+    return args;
+};
+
+exports = module.exports = {
+    setArgument, setDescription,
+};
