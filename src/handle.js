fe0ac0324e012ed466640943228c033e1a2c4d6e
Ilya <ifed@tuta.io>
2018-07-30 18:29:07 +0300

    feat: add postfixes
    - Change section name to array of names
    - Add postfixes for section names
    
    Closes #38

--- src/handle.js
+++ src/handle.js
@@ -8,13 +8,27 @@ const handle = (help, config) => {
         context.options = [];
         context.delimiter = config.delimiter;
         Object.keys(context.section).forEach((sectionName) => {
-            let section = context.section[sectionName];
-            let sectionTitle = config.section[sectionName].name;
-            const data = findSection(sectionTitle, help);
+            let contextSection = context.section[sectionName];
+            let configSection = config.section[sectionName];
+            let data = [];
+            configSection.names.some((name) => {
+                let section = findSection(name, help);
+                if (section.length > 0) {
+                    data = data.concat(section);
+                } else {
+                    configSection.postfix.some((postfix) => {
+                        section = findSection(name + postfix, help);
+                        if (section.length > 0) {
+                            data = data.concat(section);
+                            return false;
+                        }
+                    });
+                }
+            });
             if (data.length > 0) {
                 data.forEach((object) =>
-                    section.func(object, context, argumentTemplate));
-            } else if (section.required) {
+                    contextSection.func(object, context, argumentTemplate));
+            } else if (contextSection.required) {
                 throw new Error(`Required section ${sectionName} not found`);
             }
         });
@@ -27,7 +41,7 @@ const handle = (help, config) => {
 const findSection = (sectionName, help) => {
     try {
         const regularExp = new
-            RegExp(`[^\n]*${sectionName}[^\n]*\n?(?:[ \t].*?(?:\n|$))*`, 'gi');
+            RegExp(`[\\s]+${sectionName}[\n]+\n?(?:[ \t].*?(?:\n|$))*`, 'gmi');
         const matches = help.match(regularExp);
         return matches ? matches.map((match) => match.trim()) : [];
     } catch (error) {
