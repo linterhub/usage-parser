c30da2378ee2751abbedb4340907df9b7b7021df
Ilya <ifed@tuta.io>
2018-07-31 13:54:51 +0300

    test: add tests for parseWhole property
    Add 4 tests:
    - config have parseWhole property
    - config have property [parseWhole] with wrong type
    - help without options and parseWhole true
    - help with options and parseWhole true (only options should be parsed) [invalid]
    
    Fix 1 test:
    - add to without option section test config with parseWhole false value
    
    Closes #83

--- test/sections/test.js
+++ test/sections/test.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const parser = require('./../../src/parser.js');
 const chai = require('chai');
 const testsData = require('./test.json');
-const config = require('./../../src/template/configDefault.json');
+const configDefault = require('./../../src/template/configDefault.json');
 const mocha = require('mocha');
-const assert = require('assert');
 
 const main = () => {
     testsData.tests.forEach((test) => {
         mocha.it(`test ${test.description}`, (done) => {
-            runTest(test, done);
+            const config = test.config ? require(test.config) : configDefault;
+            runTest(test, config, done);
         });
     });
 };
 
-const runTest = (test, done) => {
+const runTest = (test, config, done) => {
     const doc = fs.readFileSync(test.$ref, 'utf8');
     if (test.object === 'exception') {
         const result = chai.expect(() => parser(doc, config));
